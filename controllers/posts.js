c834850819d72c72ede8cbe3f54f299a869c76d8
Miftachussurur <108846627+mftaa@users.noreply.github.com>
2023-06-06 08:38:28 +0700

    update

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,61 +1,142 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
+import express from "express";
 
+const router = express.Router();
 
+export const getPosts = async (req, res) => {
+  const { page } = req.query;
 
-export const getPosts = async (req,res)=>{
-    try {
-        const postMessage= await PostMessage.find();
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
 
-        res.status(200).json(postMessage);
-    } catch (error) {
-        res.status(404).json({message:error.message});
-        
-    }
-}
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+
+    res.json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+
+  try {
+    const title = new RegExp(searchQuery, "i");
+
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(",") } }],
+    });
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getPost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await PostMessage.findById(id);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const createPost = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
+  const post = req.body;
 
-    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+  const newPostMessage = new PostMessage({
+    ...post,
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
 
-    try {
-        await newPostMessage.save();
+  try {
+    await newPostMessage.save();
 
-        res.status(201).json(newPostMessage );
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
-}
+    res.status(201).json(newPostMessage);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
 
-export const updatePost = async (req,res)=>{
-    const {id:_id} = req.params;
-    const post=req.body
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('tidak ada post dengan id tersebut');
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("tidak ada post dengan id tersebut");
 
-    const updatedPost= await PostMessage.findByIdAndUpdate(_id,{...post,_id},{new:true});
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true }
+  );
 
-    res.json(updatedPost);
-}
+  res.json(updatedPost);
+};
 
-export const deletePost = async (req,res)=>{
-    const {id} = req.params;
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('tidak ada post dengan id tersebut');
-    await PostMessage.findByIdAndRemove(id)
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("tidak ada post dengan id tersebut");
+  await PostMessage.findByIdAndRemove(id);
 
-    console.log('DELETE!')
+  console.log("DELETE!");
 
-    res.json({message:'Post berhasil dihapus'});
-}
+  res.json({ message: "Post berhasil dihapus" });
+};
 
-export const likePost = async (req,res)=>{
-    const {id} = req.params;
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('tidak ada post dengan id tersebut');
+export const likePost = async (req, res) => {
+  const { id } = req.params;
 
-    const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id,{likeCount:post.likeCount+ 1},{new:true})
+  if (!req.userId) {
+    return res.json({ message: "Unauthenticated" });
+  }
 
-    res.json(updatedPost);
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
 
-}
+  const post = await PostMessage.findById(id);
+
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+  res.status(200).json(updatedPost);
+};
+
+export const commentPost = async (req, res) => {
+  const { id } = req.params;
+  const { value } = req.body;
+
+  const post = await PostMessage.findById(id);
+
+  post.comments.push(value);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
+};
+export default router;
--- index.js
+++ index.js
@@ -2,26 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/user.js';
+
 
 const app = express();
-dotenv.config();
-
-app.use(bodyParser.json({limit:"30mb", extended : true}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended : true}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts',postRoutes);
+app.use('/posts', postRoutes);
+app.use('/user',userRoutes)
 
-app.get('/',(req,res)=>{
-    res.send('hai ini memories API');
-});
-
-//const CONNECTION_URL = 'mongodb+srv://2wsx1qaz:2wsx1qaz@nodeapp.lm3x7.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://2wsx1qaz:2wsx1qaz@nodeapp.lm3x7.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser:true,useUnifiedTopology:true})
-    .then(()=> app.listen(PORT,()=>console.log(`server running on port: ${PORT}`)))
-    .catch((error)=>console.log(error.massage) );
+mongoose.connect(CONNECTION_URL,{ useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=> app.listen(PORT,()=>console.log(`server running on port: http://localhost: ${PORT}`)))
+    .catch((error)=>console.log('error :',error.massage) );
 
